28ff2c965d04ae3a604734381f06f6731595fa1e
C_bi__ <chinmaya@kloudle.com>
2023-09-14 15:40:52 +0530

    testing hello api

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,15 +1,23 @@
 import { useLoginState } from "@/context/login-context";
+import hello from "@/pages/api/hello";
 import Link from "next/link";
 import PencilRuler from "./icons/PencilRuler";
 import Login from "./Login";
 import NavMob from "./NavMob";
 import Register from "./Register";
+import { Button } from "./ui/button";
 import UserProfileDropdown from "./UserProfileDropdown";
 
 const Navbar = () => {
   const loginState = useLoginState();
   const isLoggedIn = loginState.isLoggedIn;
   const userEmail = loginState.email;
+
+  async function handleHello() {
+    await fetch("/api/hello").then((res) => {
+      console.log("this is hello api res", res);
+    });
+  }
 
   return (
     <nav className="self-stretch sticky top-0 backdrop-saturate-200 backdrop-blur-sm z-30 flex items-center justify-between py-3 px-4 md:px-8 bg-white/80 shadow-lg">
@@ -20,6 +28,7 @@ const Navbar = () => {
           <h1 className="font-semibold">Todo-App</h1>
         </Link>
       </div>
+      <Button onClick={handleHello}>Hello api</Button>
       {isLoggedIn ? (
         <UserProfileDropdown email={userEmail} />
       ) : (
